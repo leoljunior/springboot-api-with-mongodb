248099fa44c4fb781be70be649a094d872a57db9
leoljunior <juhhinx@gmail.com>
2022-06-17 10:29:14 -0300

    first commit

---
+++ src/main/java/com/prova/leonardojunior/LeonardoJuniorApplication.java
@@ -0,0 +1,13 @@
+package com.prova.leonardojunior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeonardoJuniorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeonardoJuniorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prova/leonardojunior/LeonardoJuniorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prova.leonardojunior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeonardoJuniorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
